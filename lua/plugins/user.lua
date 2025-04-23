293e8065db3a746215d103f17ef62bc9a28878a2
ANASTASAKIS Konstantinos <kon.anastasakis@gmail.com>
2025-04-23 12:52:31 +0300

    Initial commit

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -115,4 +115,35 @@ return {
   { "stevearc/dressing.nvim",
   event = "VeryLazy", -- only loads when needed
   },
+
+  -- === Python Debugging Support ===
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",           -- Fancy debug interface
+      "jay-babu/mason-nvim-dap.nvim",   -- Auto-installs DAP backends (like debugpy)
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "python" },
+        automatic_setup = true,
+      })
+
+      dapui.setup()
+
+      -- Auto open/close DAP UI when debugging starts or ends
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
 }
